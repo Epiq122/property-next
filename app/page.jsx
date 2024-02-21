5d0437f0a422b26912c83e37c3c4a9c870c9f089
Robert Gleason <l2ob122@gmail.com>
2024-02-20 20:42:06 -0800

    Add Footer and Hero components, and create InfoBox and InfoBoxes components

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
+import InfoBoxes from '../components/InfoBoxes';
+import Hero from '../components/Hero';
 import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 className='text-3xl'>Welcome</h1>
-      <Link href='/properties'>Show Properties</Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
   );
 };
 export default HomePage;
