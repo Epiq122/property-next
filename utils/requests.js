c50f613c120a427ab3daf1960a5c65c101dcf4ec
Robert Gleason <l2ob122@gmail.com>
2024-02-22 19:59:42 -0800

    created the fetch request for a single property

--- utils/requests.js
+++ utils/requests.js
@@ -17,4 +17,24 @@ async function fetchProperties() {
     return [];
   }
 }
-export { fetchProperties };
+
+// Fetch single property
+async function fetchProperty(id) {
+  try {
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+    // handle the case where the domain is not available
+    if (!apiDomain) {
+      return null;
+    }
+
+    if (!res.ok) {
+      throw new Error("An error occurred while fetching the data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
