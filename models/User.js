e4af11d2d12019905ca3fa5ee4facc5c25928da6
Robert Gleason <l2ob122@gmail.com>
2024-02-22 19:09:50 -0800

    created the User and Property Models

---
+++ models/User.js
@@ -0,0 +1,32 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Property",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
