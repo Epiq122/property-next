fea844ef96892857411e62e5e42e04200d4de727
Robert Gleason <l2ob122@gmail.com>
2024-02-23 19:54:43 -0800

    added session provider component, added env variables

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,25 @@
-import '@/assets/styles/globals.css';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
+import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
-  title: 'PropertyPulse | Find The Perfect Rental Property',
-  description: 'Find The Perfect Rental Property',
-  keywords: 'rental, property, find, perfect',
+  title: "PropertyPulse | Find The Perfect Rental Property",
+  description: "Find The Perfect Rental Property",
+  keywords: "rental, property, find, perfect",
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <html lang='en'>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 export default MainLayout;
